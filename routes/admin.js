a117a3e0266ac16e02ac21c265fd61ebd6c3efea
Ahmad Danang <ahmad.danang91@gmail.com>
2021-01-14 21:39:00 +0700

    crud bank

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const adminController = require("../controllers/adminController");
+const { upload } = require("../middlewares/multer");
 
 router.get("/dashboard", adminController.viewDashboard);
 
@@ -9,7 +10,12 @@ router.post("/category", adminController.addCategory);
 router.put("/category", adminController.editCategory);
 router.delete("/category/:id", adminController.deleteCategory);
 
+//enpoint bank
 router.get("/bank", adminController.viewBank);
+router.post("/bank", upload, adminController.addBank);
+router.put("/bank", upload, adminController.editBank);
+router.delete("/bank/:id", adminController.deleteBank);
+
 router.get("/item", adminController.viewItem);
 router.get("/booking", adminController.viewBooking);
 
