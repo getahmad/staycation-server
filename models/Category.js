795e55e25f1503093058cf2cebe67274ba29160c
Ahmad Danang <ahmad.danang91@gmail.com>
2021-01-20 22:29:01 +0700

    add item and show table item

--- models/Category.js
+++ models/Category.js
@@ -1,10 +1,17 @@
 const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
 
 const categorySchema = new mongoose.Schema({
   name: {
     type: String,
     require: true,
   },
+  itemId: [
+    {
+      type: ObjectId,
+      ref: "Item",
+    },
+  ],
 });
 
 module.exports = mongoose.model("Category", categorySchema);
--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const adminController = require("../controllers/adminController");
-const { upload } = require("../middlewares/multer");
+const { uploadSingle, uploadMultiple } = require("../middlewares/multer");
 
 router.get("/dashboard", adminController.viewDashboard);
 
@@ -12,11 +12,14 @@ router.delete("/category/:id", adminController.deleteCategory);
 
 //enpoint bank
 router.get("/bank", adminController.viewBank);
-router.post("/bank", upload, adminController.addBank);
-router.put("/bank", upload, adminController.editBank);
+router.post("/bank", uploadSingle, adminController.addBank);
+router.put("/bank", uploadSingle, adminController.editBank);
 router.delete("/bank/:id", adminController.deleteBank);
 
+//enpoint item
 router.get("/item", adminController.viewItem);
+router.post("/item", uploadMultiple, adminController.addItem);
+
 router.get("/booking", adminController.viewBooking);
 
 module.exports = router;
